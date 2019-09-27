3fa2444988d2f2d8a225d49ea75156b53ff4a759
Santiago MVO <santiago.mvao@gmail.com>
2019-09-27 18:49:52 -0500

    added end game conditions & page display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
+//Class that gives structure to each character object
 class characterClass{
     constructor(name, HP, AP, critChance, attIcon, specAP, specCD, specIcon, img){
         this.name = name;
+        this.maxHP = HP;
         this.HP = HP*5;
         this.AP = AP;
         this.critChance = critChance;
@@ -9,6 +11,7 @@ class characterClass{
         this.specCD = specCD;
         this.specIcon = specIcon;
         this.img = img;
+        this.lvl = 1;
     }
     get attack(){
         if(this.critChance >= randomNumber(100,1)){
@@ -22,13 +25,13 @@ class characterClass{
         if(this.critChance >= randomNumber(100,1)){
             return [randomNumber(this.specAP,this.specAP/2)*2,true];
         } else{
-            return [randomNumber(this.specAP),false];
+            return [randomNumber(this.specAP,this.specAP),false];
         }
     }
 
     generateStartCard(){
         var newCol = $("<div>");
-        newCol.attr("class", "col-md-4 col-sm-6");
+        newCol.attr("class", "col-lg-4 col-md-6");
         var newCard = $("<div>");
         newCard.attr("class", "character jumbotron");
         newCard.attr("id", this.name);
@@ -80,7 +83,7 @@ class characterClass{
     }
 
     addAttacks(){
-        $("#"+this.name).children("h5").text("Hero: "+this.name);
+        $("#"+this.name).children("h5").text(`Hero:${this.name} Lvl:${this.lvl}`);
         var newButton = $("<button>");
         newButton.attr("class", "btn btn-secondary");
         newButton.attr("id","attBttn");
@@ -113,6 +116,7 @@ class characterClass{
     }
 }
 
+//Array of objects that are constructed from characterClass
 let characters = [
     new characterClass("Archer", 6, 8, 30, "far fa-bow-arrow", 16, 4, "far fa-bullseye-arrow", "assets/images/Archer.png"),
     new characterClass("Warrior", 10, 8, 10, "far fa-sword", 10, 2, "far fa-shield-alt", "assets/images/Warrior.png"),
@@ -121,21 +125,153 @@ let characters = [
     new characterClass("Druid", 8, 8, 20, "far fa-staff", 20, 5, "far fa-paw-claws", "assets/images/Druid.png")
 ];
 
-/*characters[1].specAttack = new function(){
-    this.HP += randomNumber(this.specAP);
-    if(this.critChance >= randomNumber(100,1)){
-        console.log("SPEC-CRITICAL!");
-        return randomNumber(this.specAP)*2;
-    } else{
-        return randomNumber(this.specAP);
+//Object with properties and methods singular to game, gE = gameEngine
+let gE = {
+    selectedHero:"",
+    //hero:"",gE.hero = jQuery.extend(true, {}, characters[gE.index(gE.selectedHero)]);
+    heroCD:"",
+    selectedEnemy:"",
+    alreadySelected:[],
+    state:0,
+
+    emptyCol:function(){
+        var newCol = $("<div>");
+        newCol.attr("class", "col-lg-4 col-md-0");
+        $("#mainRow").append(newCol);
+    },
+    
+    attack:function(dmg){
+        if(dmg[1]){
+            $(".modal-title").text("Critical Hit!");
+            $(".modal-body").text(`You dealt ${dmg[0]/2} damage twice, for a total of ${dmg[0]}.`);
+        } else{
+            $(".modal-title").text("Hit!");
+            $(".modal-body").text(`You dealt ${dmg[0]} damage.`);
+        }
+        characters[gE.index(gE.selectedEnemy)].HP -= dmg[0];
+        characters[gE.index(gE.selectedEnemy)].updateHTML();
+        $('#alert').modal('show');
+    },
+    
+    specialAttack:function(dmg){
+        if(dmg[1]){
+            $(".modal-title").text("Critical Special Hit!");
+            $(".modal-body").text(`You dealt ${dmg[0]/2} damage twice with your special attack, for a total of ${dmg[0]}.`);
+        } else{
+            $(".modal-title").text("Special Hit!");
+            $(".modal-body").text(`You dealt ${dmg[0]} damage with your special attack!`);
+        }
+        characters[gE.index(gE.selectedEnemy)].HP -= dmg[0];
+        characters[gE.index(gE.selectedEnemy)].updateHTML();
+        $('#alert').modal('show');
+
+        //Disable button & color CD red
+        $("#cdSpan").css("color","darkred");
+        $("#specAttBttn").attr("disabled",true);
+    },
+    
+    counterAttack:function(dmg){
+        if(dmg[1]){
+            $('#alert2').children().children().children().children(".modal-title").text("Critical counter-hit!");
+            $('#alert2').children().children().children(".modal-body").text(`You were dealt ${dmg[0]/2} damage twice, for a total of ${dmg[0]}.`);
+        } else{
+            $('#alert2').children().children().children().children(".modal-title").text("Counter-hit!");
+            $('#alert2').children().children().children(".modal-body").text(`You were dealt ${dmg[0]} damage.`);
+        }
+        characters[gE.index(gE.selectedHero)].HP -= dmg[0];
+        characters[gE.index(gE.selectedHero)].updateHTML();
+        $('#alert2').modal('show');
+    },
+
+    cdCheck:function(){
+        if(gE.heroCD>1){
+            $("#cdSpan").text(`   CD: ${--gE.heroCD}`);
+        } else{
+            $("#cdSpan").text(`   CD: ${characters[gE.index(gE.selectedHero)].specCD}`);
+            $("#specAttBttn").attr("disabled",false);
+            $("#cdSpan").css("color","");
+            gE.heroCD = characters[gE.index(gE.selectedHero)].specCD;
+        }
+    },
+
+    index:function(value){
+        for(let i = 0 ; i < characters.length ; i++){
+            if(characters[i].name === value) return i;
+        }
+        return null;
+    },
+
+    enemyAlive:function(){
+        if(characters[gE.index(gE.selectedEnemy)].HP>0) return true;
+        else return false;
+    },
+    
+    victory:function(){
+        gE.state=3;
+        $('#alert2').children().children().children().children(".modal-title").text(`Victory!`);
+        $('#alert2').children().children().children(".modal-body").text(`You defeated the ${gE.selectedEnemy} and leveled up!`);
+        $('#alert2').modal('show');
+
+        //Leveling up
+        characters[gE.index(gE.selectedHero)].lvl++;
+        characters[gE.index(gE.selectedHero)].maxHP++;
+        characters[gE.index(gE.selectedHero)].HP=characters[gE.index(gE.selectedHero)].maxHP*5;
+        characters[gE.index(gE.selectedHero)].specAP+=2;
+        characters[gE.index(gE.selectedHero)].critChance+=10;
+    },
+
+    gameEnd:function(won){
+        var newP = $("<h2>");
+        newP.attr("class","text-center col-12");
+        if(won){
+            $("#title").text("YOU WIN!!");
+            newP.html(`
+            You defeated all ${characters.length-1} enemies!<br>
+            You reached level ${characters[this.index(this.selectedHero)].lvl}!<br>
+            <br>
+            Congratulations!!!
+            `);
+        } 
+        else{ 
+            $("#mainRow").empty();
+            $("#title").text("YOU DIED...");
+            newP.html(`
+            You only defeated ${this.alreadySelected.length-2} enemies.<br>
+            You died at level ${characters[this.index(this.selectedHero)].lvl}.<br>
+            You died by the hands of the ${this.selectedEnemy}!<br>
+            <br>
+            Try again...
+            `);
+        }
+
+        $("#mainRow").append(newP);
+    },
+
+    newEnemy:function(){
+        $("#mainRow").empty();
+        if(characters.length===this.alreadySelected.length){
+            this.gameEnd(true);
+        } else {
+            $("#title").text("Choose a new enemy to fight!");
+
+            for(let i = 0 ; i < characters.length ; i++){
+                var already = false;
+                for(let j = 0 ; j < this.alreadySelected.length ; j++){
+                    if(characters[i].name===this.alreadySelected[j]){
+                        already = true;
+                        break;
+                    }
+                }
+                if(!already){
+                    characters[i].generateStartCard();
+                    characters[i].updateHTML();
+                }
+            }
+        }
     }
-}*/
+}
 
-var selectedHero;
-var heroCD;
-var selectedEnemy;
-var gameState = 0;
-
+//Receiving clicks
 $(document).ready(function() {
 
     //Tooltop Inizialization
@@ -147,140 +283,99 @@ $(document).ready(function() {
         character.updateHTML();
     }
 
+    //Clicking on a character
+    $(document).on("click", ".character",function() {
+        if(gE.state === 0){
+            gE.selectedHero = $(this).attr('id').toString();
+            console.log("Hero: "+gE.selectedHero);
+            $(".modal-title").text("Confirm Class");
+            $(".modal-body").text(`Your class will be ${gE.selectedHero}, confirm choice?`);
+            $('#confirm').modal('show');
+        } else if(gE.state === 1){
+            gE.selectedEnemy = $(this).attr('id').toString();
+            console.log("Enemy: "+gE.selectedEnemy);
+            $(".modal-title").text("Confirm Enemy");
+            $(".modal-body").text(`Your enemy will be ${gE.selectedEnemy}, confirm choice?`);
+            $('#confirm').modal('show');
+        }
+    });
+
+    //Clicking yes on Confirm (Choosing Hero and Enemies)
+    $('#confirm').on('click', '#btnYes', function(){
+        if(gE.state===0){
+            console.log("Accepted Hero: " + gE.selectedHero);
+            gE.heroCD = characters[gE.index(gE.selectedHero)].specCD;
+            gE.state=1;
+            $(`#${gE.selectedHero}`).parent().remove();;
+            $("#title").text("Choose an enemy to fight!");
+            gE.alreadySelected.push(gE.selectedHero);
+        } else if(gE.state===1){
+            console.log("Accepted enemy: " + gE.selectedEnemy);
+            gE.state=2;
+            $("#mainRow").empty();
+            $("#title").text("Fight!");
+            characters[gE.index(gE.selectedHero)].generateStartCard();
+            characters[gE.index(gE.selectedHero)].updateHTML();
+            characters[gE.index(gE.selectedHero)].addAttacks();
+            gE.emptyCol();
+            characters[gE.index(gE.selectedEnemy)].generateStartCard();
+            characters[gE.index(gE.selectedEnemy)].updateHTML();
+            $(`#${gE.selectedEnemy}`).children("h5").text(`Enemy: ${gE.selectedEnemy}`);
+            gE.alreadySelected.push(gE.selectedEnemy);
+        }
+    });
+
     //Attacking
     $(document).on("click","#attBttn",function(){
         //Hero damages enemy
-        var dmg = characters[indexChar(selectedHero)].attack;
-        if(dmg[1]){
-            $(".modal-title").text("Critical Hit!");
-            $(".modal-body").text(`You dealt ${dmg[0]/2} damage twice, for a total of ${dmg[0]}.`);
-        } else{
-            $(".modal-title").text("Hit!");
-            $(".modal-body").text(`You dealt ${dmg[0]} damage.`);
-        }
-        characters[indexChar(selectedEnemy)].HP -= dmg[0];
-        characters[indexChar(selectedEnemy)].updateHTML();
+        gE.attack(characters[gE.index(gE.selectedHero)].attack);
 
-        //Enemy damages hero
-        var dmg2 = characters[indexChar(selectedEnemy)].attack;
-        console.log(dmg2[0]);
-        if(dmg2[1]){
-            $('#alert2').children().children().children().children(".modal-title").text("Critical counter-hit!");
-            $('#alert2').children().children().children(".modal-body").text(`You were dealt ${dmg2[0]/2} damage twice, for a total of ${dmg2[0]}.`);
-        } else{
-            $('#alert2').children().children().children().children(".modal-title").text("Counter-hit!");
-            $('#alert2').children().children().children(".modal-body").text(`You were dealt ${dmg2[0]} damage.`);
-        }
-        characters[indexChar(selectedHero)].HP -= dmg2[0];
-        characters[indexChar(selectedHero)].updateHTML();
+        //Check if enemy died
+        if(gE.enemyAlive()){
+            //Enemy damages hero
+            gE.counterAttack(characters[gE.index(gE.selectedEnemy)].attack);
 
-        //Alerts
-        $('#alert').modal('show');
-        $('#alert2').modal('show');
-
-        //Special Attack CD
-        if(heroCD>1){
-            $("#cdSpan").text(`   CD: ${--heroCD}`);
+            //Check if died
+            if(characters[gE.index(gE.selectedHero)].HP <= 0) gE.gameEnd(false);
         } else{
-            $("#cdSpan").text(`   CD: ${characters[indexChar(selectedHero)].specCD}`);
-            $("#specAttBttn").attr("disabled",false);
-            $("#cdSpan").css("color","");
-            heroCD = characters[indexChar(selectedHero)].specCD;
+            //Select new enemy
+            gE.victory();
         }
+        
+        //Decrease special attack CD & refreshes it if done
+        gE.cdCheck();
     });
+    
 
     //Special Attacking
     $(document).on("click","#specAttBttn",function(){
-        //Hero damages enemy
-        var dmg = characters[indexChar(selectedHero)].specAttack;
-        if(dmg[1]){
-            $(".modal-title").text("Critical Special Hit!");
-            $(".modal-body").text(`You dealt ${dmg[0]/2} damage twice with your special attack, for a total of ${dmg[0]}.`);
+        //Hero damages enemy & triggers CD
+        gE.specialAttack(characters[gE.index(gE.selectedHero)].specAttack);
+
+        //Check if enemy died
+        if(gE.enemyAlive()){
+            //Enemy damages hero
+            gE.counterAttack(characters[gE.index(gE.selectedEnemy)].attack);
+
+            //Check if died
+            if(characters[gE.index(gE.selectedHero)].HP <= 0) gE.gameEnd(false);
         } else{
-            $(".modal-title").text("Special Hit!");
-            $(".modal-body").text(`You dealt ${dmg[0]} damage with your special attack!`);
-        }
-        characters[indexChar(selectedEnemy)].HP -= dmg[0];
-        characters[indexChar(selectedEnemy)].updateHTML();
-        $("#cdSpan").text(`   CD: ${heroCD}`);
-        $("#cdSpan").css("color","darkred");
-        $("#specAttBttn").attr("disabled",true);
-
-        //Enemy damages hero
-        var dmg2 = characters[indexChar(selectedEnemy)].attack;
-        console.log(dmg2[0]);
-        if(dmg2[1]){
-            $('#alert2').children().children().children().children(".modal-title").text("Critical counter-hit!");
-            $('#alert2').children().children().children(".modal-body").text(`You were dealt ${dmg2[0]/2} damage twice, for a total of ${dmg2[0]}.`);
-        } else{
-            $('#alert2').children().children().children().children(".modal-title").text("Counter-hit!");
-            $('#alert2').children().children().children(".modal-body").text(`You were dealt ${dmg2[0]} damage.`);
-        }
-        characters[indexChar(selectedHero)].HP -= dmg2[0];
-        characters[indexChar(selectedHero)].updateHTML();
-
-        //Alerts
-        $('#alert').modal('show');
-        $('#alert2').modal('show');
-    });
-
-    //Clicking a character
-    $(document).on("click", ".character",function() {
-        if(gameState === 0){
-            selectedHero = $(this).attr('id').toString();
-            console.log("Hero: "+selectedHero);
-            $(".modal-title").text("Confirm Class");
-            $(".modal-body").text(`Your class will be ${selectedHero}, confirm choice?`);
-            $('#confirm').modal('show');
-        } else if(gameState === 1){
-            selectedEnemy = $(this).attr('id').toString();
-            console.log("Enemy: "+selectedEnemy);
-            $(".modal-title").text("Confirm Enemy");
-            $(".modal-body").text(`Your enemy will be ${selectedEnemy}, confirm choice?`);
-            $('#confirm').modal('show');
+            //Select new enemy
+            gE.victory();
         }
     });
 
-    //Clicking yes on Confirm
-    $('#confirm').on('click', '#btnYes', function(){
-        if(gameState===0){
-            console.log("Accepted Hero: " + selectedHero);
-            heroCD = characters[indexChar(selectedHero)].specCD;
-            gameState=1;
-            $(`#${selectedHero}`).parent().remove();;
-            $("#title").text("Choose an enemy to fight!");
-        } else if(gameState===1){
-            console.log("Accepted enemy: " + selectedEnemy);
-            gameState=2;
-            $("#mainRow").empty();
-            $("#title").text("Fight!");
-            characters[indexChar(selectedHero)].generateStartCard();
-            characters[indexChar(selectedHero)].updateHTML();
-            characters[indexChar(selectedHero)].addAttacks();
-            emptyCol();
-            characters[indexChar(selectedEnemy)].generateStartCard();
-            characters[indexChar(selectedEnemy)].updateHTML();
-            $(`#${selectedEnemy}`).children("h5").text(`Enemy: ${selectedEnemy}`);
+    //Check if defeated an enemy
+    $(document).on('hide.bs.modal','#alert2', function () {
+        if(gE.state === 3){
+            gE.newEnemy();
+            gE.state = 1;
         }
     });
-
 
 
 });
 
-function indexChar(value){
-    for(let i = 0 ; i < characters.length ; i++){
-        if(characters[i].name === value) return i;
-    }
-    return null;
-}
-
 function randomNumber (max=10, min=1){
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function emptyCol(){
-    var newCol = $("<div>");
-    newCol.attr("class", "col-md-4 col-sm-6");
-    $("#mainRow").append(newCol);
 }
